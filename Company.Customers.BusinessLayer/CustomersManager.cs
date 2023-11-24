6e294e336e1652252520933ab3e4a030f572ed9f
Gonzalo <gonzalo.guillen.rivera@outlook.com>
2023-11-24 00:11:02 -0600

    Addition of ErrorLogSingleton in CustomerManager

--- Company.Customers.BusinessLayer/CustomersManager.cs
+++ Company.Customers.BusinessLayer/CustomersManager.cs
@@ -10,17 +10,22 @@ namespace Company.Customers.BusinessLayer
 {
     public class CustomersManager
     {
+        private readonly ErrorLogSingleton _errorLogSingleton;
+        public CustomersManager() {
+            _errorLogSingleton = ErrorLogSingleton.GetInstance();
+        }
+
         public bool CreateCustomer(CustomerEntity customer)
         {
             try
             {
-                throw new NotImplementedException();
+                throw new NotImplementedException(); //exception thrown for testing the Singleton
+                //login for creating a customer should be here
                 return true;
             }
             catch (Exception ex)
             {
-                ErrorLogSingleton errorLogSingleton = ErrorLogSingleton.GetInstance();
-                errorLogSingleton.SaveError(new ErrorLogEntity() { Message = ex.Message, StackTrace = ex.StackTrace! });
+                _errorLogSingleton.SaveError(new ErrorLogEntity() { Message = ex.Message, StackTrace = ex.StackTrace! });
                 return false;
             }
             
